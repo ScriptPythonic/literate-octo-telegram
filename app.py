90239a8755f9156a06897384628ad47cfc23dce0
QUADRI BASIT AYOMIDE <115375709+ScriptPythonic@users.noreply.github.com>
2024-10-14 21:56:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = create_app()
 def page_not_found(e):
     return render_template('404.html'), 404
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=False, host='0.0.0.0', port=3000)
